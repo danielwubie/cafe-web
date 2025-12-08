1a0be01e5e9a3bc4b334006e6af648cdb4963ce0
isle27 <betelhemwubie28@gmail.com>
2025-12-08 04:28:41 -0800

    Update config.py

--- cafe-nova/backend/app/config.py
+++ cafe-nova/backend/app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost:5432/cafe_fausse')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg://user:password@localhost:5432/cafe_fausse')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TABLE_LIMIT_PER_HOUR = int(os.getenv('TABLE_LIMIT_PER_HOUR', 30))
 
